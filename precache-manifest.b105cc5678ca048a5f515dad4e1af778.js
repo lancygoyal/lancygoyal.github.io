173b985137ae937337c89d6768b439bf2ef12675
Lancy Goyal <lancy.goyal@spicemoney.com>
2019-02-26 09:02:55 +0530

    Updates

---
+++ precache-manifest.b105cc5678ca048a5f515dad4e1af778.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "20dfa5190a59fd26f847",
+    "url": "/static/js/main.20dfa519.chunk.js"
+  },
+  {
+    "revision": "2a09386069b35e92873c",
+    "url": "/static/js/2.2a093860.chunk.js"
+  },
+  {
+    "revision": "20dfa5190a59fd26f847",
+    "url": "/static/css/main.07318036.chunk.css"
+  },
+  {
+    "revision": "51153b1b46ee3e5cde4067dc64a28357",
+    "url": "/index.html"
+  }
+];
