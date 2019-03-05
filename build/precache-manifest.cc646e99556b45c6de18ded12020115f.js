6e2df4a05c9859ebda8e93f606e13d5dd8b868c2
Lancy Goyal <lancy.goyal@spicemoney.com>
2019-03-05 19:12:17 +0530

    minor fix

--- build/precache-manifest.cc646e99556b45c6de18ded12020115f.js
+++ build/precache-manifest.cc646e99556b45c6de18ded12020115f.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/static/js/runtime~main.fdfcfda2.js"
   },
   {
-    "revision": "20dfa5190a59fd26f847",
-    "url": "/static/js/main.20dfa519.chunk.js"
+    "revision": "73e2d6008faa48141b73",
+    "url": "/static/js/main.73e2d600.chunk.js"
   },
   {
     "revision": "2a09386069b35e92873c",
     "url": "/static/js/2.2a093860.chunk.js"
   },
   {
-    "revision": "20dfa5190a59fd26f847",
+    "revision": "73e2d6008faa48141b73",
     "url": "/static/css/main.07318036.chunk.css"
   },
   {
-    "revision": "51153b1b46ee3e5cde4067dc64a28357",
+    "revision": "2a6e9524b1d1b12246318fdfd895d004",
     "url": "/index.html"
   }
 ];
