6fbec1c2f017f05628dd32f4e0b0dfbb76795e32
Lancy Goyal <lancy.goyal@spicemoney.com>
2019-03-05 19:11:42 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b105cc5678ca048a5f515dad4e1af778.js"
+  "/precache-manifest.cc646e99556b45c6de18ded12020115f.js"
 );
 
 workbox.clientsClaim();
