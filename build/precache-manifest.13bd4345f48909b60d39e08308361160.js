27d2cf292e7858dfccd33bec6999ff5ca57b4416
Lancy Goyal <i.lancygoyal@gmail.com>
2020-01-17 13:15:31 +0400

    Build Master

---
+++ build/precache-manifest.13bd4345f48909b60d39e08308361160.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0de82a7b46021925fc48",
+    "url": "/static/css/main.07318036.chunk.css"
+  },
+  {
+    "revision": "0de82a7b46021925fc48",
+    "url": "/static/js/main.0de82a7b.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "2a09386069b35e92873c",
+    "url": "/static/js/2.2a093860.chunk.js"
+  },
+  {
+    "revision": "6884fd642c599db83e0fcb1f7d152c67",
+    "url": "/index.html"
+  }
+];
