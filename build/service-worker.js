f91dc87a3dc54b52d70cd024496f7ac85fc75e4b
Lancy Goyal <i.lancygoyal@gmail.com>
2020-02-28 13:43:20 +0530

    Passionate FullStack Software Engineer & Techpreneur.

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c73aade160e87a6ecf6ce0c2b5993b74.js"
+  "/precache-manifest.7ab6bde9922fe4ff404f682f36ed2b93.js"
 );
 
 workbox.clientsClaim();
--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -23,9 +23,9 @@ class Footer extends Component {
             <ul className="copyright">
               <li>&copy; Copyright 2020 Lancy Goyal</li>
               <li>
-                Design by{" "}
-                <a title="Styleshout" href="http://www.styleshout.com/">
-                  Styleshout
+                Made in{" "}
+                <a title="React.js" href="https://reactjs.org/">
+                  React.js
                 </a>
               </li>
             </ul>
