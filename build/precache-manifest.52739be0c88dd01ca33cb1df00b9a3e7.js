015fc994423c423af4d8ba3a5b8a1f5adf72faef
Lancy <i.lancygoyal@gmail.com>
2022-02-16 19:10:25 +0530

    build revamp feb 2022

---
+++ build/precache-manifest.52739be0c88dd01ca33cb1df00b9a3e7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "62c53232267e83562d52",
+    "url": "/static/css/main.07318036.chunk.css"
+  },
+  {
+    "revision": "62c53232267e83562d52",
+    "url": "/static/js/main.62c53232.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "2a09386069b35e92873c",
+    "url": "/static/js/2.2a093860.chunk.js"
+  },
+  {
+    "revision": "7428022e4ab7527658f737ca4c1f5591",
+    "url": "/index.html"
+  }
+];
